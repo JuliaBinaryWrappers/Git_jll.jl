5b5880857ed334afda66d2d99781d51fcabacbaa
jlbuild <juliabuildbot@gmail.com>
2025-11-29 16:28:12 +0000

    Git_jll build 2.52.0+0

--- src/Git_jll.jl
+++ src/Git_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Git")
-JLLWrappers.@generate_main_file("Git", UUID("f8c6e375-362e-5223-8a59-34ff63f689eb"))
+JLLWrappers.@generate_main_file("Git", Base.UUID("f8c6e375-362e-5223-8a59-34ff63f689eb"))
 end  # module Git_jll
